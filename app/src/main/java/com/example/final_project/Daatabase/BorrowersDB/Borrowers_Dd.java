0c4aeeb3fcd3f50a6a229219421621ac19c7ae77
kartik_panjla <KARTIk$9816>
2020-07-19 06:57:17 +0530

    udpate database but error ocuur wile inserting

--- app/src/main/java/com/example/final_project/Daatabase/BorrowersDB/Borrowers_Dd.java
+++ app/src/main/java/com/example/final_project/Daatabase/BorrowersDB/Borrowers_Dd.java
@@ -1,28 +1,33 @@
 package com.example.final_project.Daatabase.BorrowersDB;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.final_project.Contact;
+
 public class Borrowers_Dd extends SQLiteOpenHelper {
+    private static String DATABASE_NAME="Borrower";
+    private static int DATABASE_VERSION=1;
     private String TABLE="Borrower";
     private String COLUMN_1="ID";
     private String COLUMN_2="E-mail";
     private String COLUMN_3="AMMOUNT";
+    SQLiteDatabase db;
 
+    public Borrowers_Dd( add_borrower context) {
 
-
-
-
-
-    public Borrowers_Dd( Context context) {
-
-            super(context, "Borrower_Db", null, 1);
+            super(context, DATABASE_NAME, null,DATABASE_VERSION);
         }
          @Override
         public void onCreate(SQLiteDatabase db) {
-           // String Sql="CREATE TABLE"+TABLE+"("+COLUMN_1+"INTEGER PRIMARY KEY"+COLUMN_2+"TEXT"+COLUMN_3+"TEXT"+")";
-           // db.execSQL(Sql);
+
+          String sql="create table "+TABLE+"(ID INTEGER PRIMARY KEY,NAME TEXT,AMMOUNT TEXT);";
+
+           db.execSQL(sql);
+
+
 
         }
 
@@ -32,6 +37,18 @@ public class Borrowers_Dd extends SQLiteOpenHelper {
         onCreate(db);
 
         }
+
+    public long saveContact(Contact contact) {
+        db=getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(COLUMN_1,contact.getId());
+        values.put(COLUMN_2,contact.getMail());
+        values.put(COLUMN_3,contact.getAmount());
+
+        long rowCount=db.insert(TABLE,null,values);
+        db.close();
+        return rowCount;
     }
+}
 
 
