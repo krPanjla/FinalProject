562d52081e8618950920fb7e3689693f1c4e2481
PARASF670 <42441544+PARASF670@users.noreply.github.com>
2020-10-18 20:24:13 +0530

    Updated maps

--- maps.cpp
+++ maps.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-    /*pair<int, int> p = make_pair(6,21);
-    cout << p.first << "    " << p.second;*/
+    pair<int, int> p = make_pair(6,21);
+    cout << p.first << "    " << p.second;
     multimap<int, int> m;
-   // m[1] = 33;
-   // m[4] = 45;
+   m[1] = 33;
+   m[4] = 45;
     m.insert(make_pair(6, 200));
     
     for (auto it = m.begin(); it != m.end();it++)
